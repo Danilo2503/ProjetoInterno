e282e6cdbd5aa0d59e0a1d284448fb2300873aa8
Danilo Rocha <danilo.rocha5@senaisp.edu.br>
2021-09-16 11:24:49 -0300

    Turma repository

---
+++ Carometro.webApi/Carometro.webApi/Repositories/TurmaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Carometro.webApi.Domains;
+using Carometro.webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Carometro.webApi.Repositories
+{
+    public class TurmaRepository : ITurmaRepository
+    {
+        public void Atualizar(int id, Turma turmaAtualizada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Turma novaTurma)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Turma> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Carometro.webApi/Carometro.webApi/Repositories/UsuarioRepository.cs
+++ Carometro.webApi/Carometro.webApi/Repositories/UsuarioRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Carometro.webApi.Contexts;
+using Carometro.webApi.Domains;
 using Carometro.webApi.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -11,11 +12,70 @@ namespace Carometro.webApi.Repositories
     {
         CarometroContext ctx = new CarometroContext();
 
-        public void Cadastrar(UsuarioRepository novoUsuario)
+        public void Atualizar(int id, Usuario usuarioAtualizado)
+        {
+            Usuario usuarioBuscado = ctx.Usuarios.Find(id);
+
+            if(usuarioAtualizado.Nome != null)
+            {
+                usuarioBuscado.Nome = usuarioAtualizado.Nome;
+            }
+
+            if(usuarioAtualizado.Email != null)
+            {
+                usuarioBuscado.Email = usuarioAtualizado.Email;
+            }
+
+            if(usuarioAtualizado.Senha != null)
+            {
+                usuarioBuscado.Senha = usuarioAtualizado.Senha;
+            }
+
+            if (usuarioAtualizado.IdTipoUsuario != 0)
+            {
+                usuarioBuscado.IdTipoUsuario = usuarioAtualizado.IdTipoUsuario;
+            }
+
+            ctx.Usuarios.Update(usuarioAtualizado);
+
+            ctx.SaveChanges();
+        }
+
+        public void Cadastrar(Usuario novoUsuario)
         {
             ctx.Usuarios.Add(novoUsuario);
 
             ctx.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Usuario usuarioBuscado = ctx.Usuarios.Find(id);
+
+            ctx.Usuarios.Remove(usuarioBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Usuario> Listar()
+        {
+            return ctx.Usuario
+
+            .include(m => m.idSalaNavigation)
+
+            .Select(m => new Usuario
+            {
+                IdUsuario = m.IdUsuario,
+                Nome = m.Nome,
+
+                IdTipoUsuarioNavigation = new TipoUsuario
+            {
+                IdTipoUsuario = m.IdTipoUsuarioNavigation.IdTipoUsuario,
+                Nome = m.IdTipoUsuarioNavigation.Nome
+            }
+
+            })
+                .ToList();
         }
     }
 }
